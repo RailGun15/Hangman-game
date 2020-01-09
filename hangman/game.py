943565057c1eca7099c43e9d0bdcccc3c2585791
Ignacio Keegan <46688572+RailGun15@users.noreply.github.com>
2020-01-08 21:55:40 -0300

    Add files via upload

--- hangman/game.py
+++ hangman/game.py
@@ -1,23 +1,51 @@
 from .exceptions import *
+import random
 
 # Complete with your own, just for fun :)
-LIST_OF_WORDS = []
+LIST_OF_WORDS = ["lion", "umbrella", "window", "computer", "glass", "juice", "chair", "desktop",
+ "laptop", "angel","dog","shotgun","tiger", "phantom", "cat", "lemon", "cabel", "mirror", "hat","car","python"]
 
 
 def _get_random_word(list_of_words):
-    pass
+    if list_of_words != []:
+        return random.choice(list_of_words)
+    else:
+        raise InvalidListOfWordsException()
 
 
 def _mask_word(word):
-    pass
+    if word != "":
+        masked_word = "".join(["*" for letter in word])
+        return masked_word
+    else:
+        raise InvalidWordException()
 
 
 def _uncover_word(answer_word, masked_word, character):
-    pass
-
+    if len(character) > 1:
+        raise InvalidGuessedLetterException()
+    elif answer_word == "" or masked_word == "":
+        raise InvalidWordException()
+    elif len(answer_word) != len(masked_word):
+        raise InvalidWordException()
+    hidden_word = "".join(answer_word[idx].lower() if character.lower() == answer_word[idx].lower() != -1 else masked_word[idx] for idx,letter in enumerate(answer_word))
+    return hidden_word
 
 def guess_letter(game, letter):
-    pass
+    if game["masked_word"] == game["answer_word"]:
+            raise GameFinishedException()
+    elif game["remaining_misses"] == 0:
+            raise GameFinishedException()
+    elif letter.lower() in game["answer_word"].lower():
+        game["previous_guesses"].append(letter.lower())
+        game["masked_word"] = _uncover_word(game["answer_word"],game["masked_word"],letter)
+        if game["masked_word"] == game["answer_word"]:
+            raise GameWonException()
+    else:
+        game["previous_guesses"].append(letter.lower())
+        game["remaining_misses"] -= 1
+        if game["remaining_misses"] == 0:
+            raise GameLostException()
 
 
 def start_new_game(list_of_words=None, number_of_guesses=5):
